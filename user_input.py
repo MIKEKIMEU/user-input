929782ba614b3114c152b139371e306804fb3e5d
MIKEKIMEU <muiamichael234@gmail.com>
2024-08-12 17:40:08 +0300

    full stop

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = input("Enter your name: ")
 age = input("What is your age: ")
 location = input("What is your location: ")
 
-print(f"Hello {name},you are {age} years old and you live in {location}")
+print(f"Hello {name},you are {age} years old and you live in {location}.")
